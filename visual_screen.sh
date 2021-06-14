1287ae33c38d21c527eb515053808cf29e8769c2
Zhuang Li <zhuangli200@gmail.com>
2021-06-13 22:25:46 -0400

    bash script

--- visual_screen.sh
+++ visual_screen.sh
@@ -8,7 +8,7 @@ EMAN2_EXE='e2proc2d.py'
 # Test if e2proc2d.py from eman2 is callable
 if ! command -v $EMAN2_EXE &> /dev/null
 then
-    echo "e2proc2d.py could not be found"
+    echo "e2proc2d.py could not be found, exiting..."
     exit
 fi
 
